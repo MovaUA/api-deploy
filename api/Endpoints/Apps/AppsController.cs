415ab311a2d28055d3e015a05383d50425249465
Valeriy Molchanov <vmo@ciklum.com>
2020-06-19 22:11:54 +0300

    AppsController

---
+++ api/Endpoints/Apps/AppsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Endpoints
+{
+    [ApiController]
+    public class AppsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("v1/apps")]
+        public IActionResult Get()
+        {
+            return Ok("apps v1");
+        }
+    }
+}
