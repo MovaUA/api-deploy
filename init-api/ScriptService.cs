4d8635297d87543e776915f71a5cf1eb8f53b793
mova <valeriy.molchanov.77@gmail.com>
2020-06-22 08:49:45 +0300

    ScriptService (draft)

--- init-api/ScriptService.cs
+++ init-api/ScriptService.cs
@@ -10,17 +10,32 @@ namespace Init.Api
   {
     private readonly IMongoCollection<DbVersion> versions;
     private readonly IReadOnlyDictionary<int, IScript> scripts;
+    private readonly IHostApplicationLifetime appLifetime;
 
     public ScriptService(
       IMongoCollection<DbVersion> versions,
-      IReadOnlyDictionary<int, IScript> scripts
+      IReadOnlyDictionary<int, IScript> scripts,
+      IHostApplicationLifetime appLifetime
     )
     {
       this.versions = versions;
       this.scripts = scripts;
+      this.appLifetime = appLifetime;
     }
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+      try
+      {
+        return ApplyScripts(stoppingToken);
+      }
+      finally
+      {
+        this.appLifetime.StopApplication();
+      }
+    }
+
+    private Task ApplyScripts(CancellationToken cancellationToken)
     {
       return Task.CompletedTask;
     }
