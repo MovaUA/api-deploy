1bd2903622eff7839d6d511b1ea1444f894c336b
mova <valeriy.molchanov.77@gmail.com>
2020-06-23 19:02:18 +0300

    init-api: exit code is set

--- init-api/Business/ScriptService.cs
+++ init-api/Business/ScriptService.cs
@@ -8,70 +8,71 @@ using Newtonsoft.Json;
 
 namespace Init.Api.Business
 {
-	public class ScriptService : BackgroundService
-	{
-		private readonly ILogger<ScriptService> logger;
-		private readonly IVersionRepository versionRepository;
-		private readonly IScriptRegistry scripts;
-		private readonly IHostApplicationLifetime appLifetime;
+  public class ScriptService : BackgroundService
+  {
+    private readonly ILogger<ScriptService> logger;
+    private readonly IVersionRepository versionRepository;
+    private readonly IScriptRegistry scripts;
+    private readonly IHostApplicationLifetime appLifetime;
 
-		public ScriptService(
-			[NotNull] ILogger<ScriptService> logger,
-			[NotNull] IVersionRepository versionRepository,
-			[NotNull] IScriptRegistry scripts,
-			[NotNull] IHostApplicationLifetime appLifetime
-		)
-		{
-			this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
-			this.versionRepository = versionRepository ?? throw new ArgumentNullException(nameof(versionRepository));
-			this.scripts = scripts ?? throw new ArgumentNullException(nameof(scripts));
-			this.appLifetime = appLifetime ?? throw new ArgumentNullException(nameof(appLifetime));
-		}
+    public ScriptService(
+      [NotNull] ILogger<ScriptService> logger,
+      [NotNull] IVersionRepository versionRepository,
+      [NotNull] IScriptRegistry scripts,
+      [NotNull] IHostApplicationLifetime appLifetime
+    )
+    {
+      this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+      this.versionRepository = versionRepository ?? throw new ArgumentNullException(nameof(versionRepository));
+      this.scripts = scripts ?? throw new ArgumentNullException(nameof(scripts));
+      this.appLifetime = appLifetime ?? throw new ArgumentNullException(nameof(appLifetime));
+    }
 
-		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-		{
-			try
-			{
-				this.logger.LogInformation(1000, "Applying scripts...");
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+      try
+      {
+        this.logger.LogInformation(1000, "Applying scripts...");
 
-				await ApplyScripts(stoppingToken);
+        await ApplyScripts(stoppingToken);
 
-				this.logger.LogInformation(1001, "Scripts applied");
-			}
-			catch (Exception exception)
-			{
-				this.logger.LogError(1002, exception, "An error occurred while applying scripts");
-				throw;
-			}
-			finally
-			{
-				this.appLifetime.StopApplication();
-			}
-		}
+        this.logger.LogInformation(1001, "Scripts applied");
+      }
+      catch (Exception exception)
+      {
+        Environment.ExitCode = 1;
+        this.logger.LogError(1002, exception, "An error occurred while applying scripts");
+        throw;
+      }
+      finally
+      {
+        this.appLifetime.StopApplication();
+      }
+    }
 
-		private async Task ApplyScripts(CancellationToken cancellationToken)
-		{
-			var latest = await this.versionRepository.FindLatest(cancellationToken).ConfigureAwait(false);
+    private async Task ApplyScripts(CancellationToken cancellationToken)
+    {
+      var latest = await this.versionRepository.FindLatest(cancellationToken).ConfigureAwait(false);
 
-			this.logger.LogInformation(1003, "Latest version: {0}", JsonConvert.SerializeObject(latest));
+      this.logger.LogInformation(1003, "Latest version: {0}", JsonConvert.SerializeObject(latest));
 
-			foreach (var script in this.scripts.GetNext(latest?.Version))
-			{
-				this.logger.LogInformation(1004, "Applying script... version: {0}", script.Version);
+      foreach (var script in this.scripts.GetNext(latest?.Version))
+      {
+        this.logger.LogInformation(1004, "Applying script... version: {0}", script.Version);
 
-				await script.Apply(cancellationToken).ConfigureAwait(false);
+        await script.Apply(cancellationToken).ConfigureAwait(false);
 
-				var newVersion =
-					new DbVersion
-					{
-						Id = Guid.NewGuid().ToString("N"),
-						Version = script.Version
-					};
+        var newVersion =
+          new DbVersion
+          {
+            Id = Guid.NewGuid().ToString("N"),
+            Version = script.Version
+          };
 
-				await this.versionRepository.Insert(newVersion, cancellationToken).ConfigureAwait(false);
+        await this.versionRepository.Insert(newVersion, cancellationToken).ConfigureAwait(false);
 
-				this.logger.LogInformation(1005, "Script applied: version: {0}", script.Version);
-			}
-		}
-	}
+        this.logger.LogInformation(1005, "Script applied: version: {0}", script.Version);
+      }
+    }
+  }
 }
--- init-api/Program.cs
+++ init-api/Program.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics.CodeAnalysis;
 using Init.Api.Business;
 using Init.Api.Scripts;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
