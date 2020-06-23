848e4b2d1a2bcfa992c1cb88ffecc41ff8793914
mova <valeriy.molchanov.77@gmail.com>
2020-06-23 17:20:54 +0300

    api/ core/runtime

--- init-api/Business/MongoDbExtensions.cs
+++ init-api/Business/MongoDbExtensions.cs
@@ -2,55 +2,60 @@ using System;
 using JetBrains.Annotations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
+using Newtonsoft.Json;
 
 namespace Init.Api
 {
-	public static class MongoDbExtensions
-	{
-		public static IServiceCollection AddMongoDb(
-			[NotNull] this IServiceCollection services,
-			[NotNull] IConfiguration configuration,
-			[NotNull] string section
-		)
-		{
-			if (services == null) throw new ArgumentNullException(nameof(services));
-			if (configuration == null) throw new ArgumentNullException(nameof(configuration));
-			if (section == null) throw new ArgumentNullException(nameof(section));
+  public static class MongoDbExtensions
+  {
+    public static IServiceCollection AddMongoDb(
+      [NotNull] this IServiceCollection services,
+      [NotNull] IConfiguration configuration,
+      [NotNull] string section
+    )
+    {
+      if (services == null) throw new ArgumentNullException(nameof(services));
+      if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+      if (section == null) throw new ArgumentNullException(nameof(section));
 
-			services.AddSingleton<IMongoDbSettings>(sp => configuration.GetSection(section).Get<MongoDbSettings>());
+      services.AddSingleton<IMongoDbSettings>(sp => configuration.GetSection(section).Get<MongoDbSettings>());
 
-			return services.AddSingleton(
-				sp =>
-				{
-					var mongo = sp.GetRequiredService<IMongoDbSettings>();
+      return services.AddSingleton(
+        sp =>
+        {
+          var mongo = sp.GetRequiredService<IMongoDbSettings>();
 
-					return new MongoClient(
-							new MongoClientSettings
-							{
-								Credential = MongoCredential.CreateCredential(
-									mongo.AuthDb,
-									mongo.User,
-									mongo.Password
-								),
-								Server = new MongoServerAddress(
-									mongo.Host,
-									mongo.Port
-								),
-								ServerSelectionTimeout = TimeSpan.FromMilliseconds(mongo.SelectTimeoutMS)
-							}
-						)
-						.GetDatabase(mongo.Db);
-				}
-			);
-		}
+          var logger = sp.GetRequiredService<ILogger<IMongoDbSettings>>();
+          logger.LogInformation(1000, JsonConvert.SerializeObject(mongo));
 
-		public static IServiceCollection AddCollection<T>([NotNull] this IServiceCollection services, [NotNull] string name)
-		{
-			if (services == null) throw new ArgumentNullException(nameof(services));
-			if (name == null) throw new ArgumentNullException(nameof(name));
+          return new MongoClient(
+              new MongoClientSettings
+              {
+                Credential = MongoCredential.CreateCredential(
+                  mongo.AuthDb,
+                  mongo.User,
+                  mongo.Password
+                ),
+                Server = new MongoServerAddress(
+                  mongo.Host,
+                  mongo.Port
+                ),
+                ServerSelectionTimeout = TimeSpan.FromMilliseconds(mongo.SelectTimeoutMS)
+              }
+            )
+            .GetDatabase(mongo.Db);
+        }
+      );
+    }
 
-			return services.AddSingleton(sp => sp.GetRequiredService<IMongoDatabase>().GetCollection<T>(name));
-		}
-	}
+    public static IServiceCollection AddCollection<T>([NotNull] this IServiceCollection services, [NotNull] string name)
+    {
+      if (services == null) throw new ArgumentNullException(nameof(services));
+      if (name == null) throw new ArgumentNullException(nameof(name));
+
+      return services.AddSingleton(sp => sp.GetRequiredService<IMongoDatabase>().GetCollection<T>(name));
+    }
+  }
 }
